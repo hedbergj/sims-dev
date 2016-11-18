37e20a57fa7b4801b51b37af09636a07497d78a9
hedbergj <james@smile2.local>
2016-11-18 08:56:42 -0500

    fixes

--- spring-vectors/sketch.js
+++ spring-vectors/sketch.js
@@ -1,13 +1,33 @@
 
-
-
+//Buttons
+var playButton;
+//Boolean to toggle animation
+var play;
+var mass, springk, amplitude, mu;
 function setup() {
 
   canvas = createCanvas(500, 400);
   canvas.parent('sketch-holder');
   frameRate(30);
+  play = true;
+
+  //Create sliders and buttons
+  playButton = createButton('pause');
+  playButton.position(20,25);
+  playButton.mousePressed(togglePlayButton);
+  playButton.value = true;
+  playButton.class("sim-button gray slim");
+  playButton.parent('sketch-holder');
+
+
   //Spring(pos_, k_, m_, relaxedLength_, oscAmp_)
-  spring = new Spring(createVector(10,height/2),30,5,250,.5,0);
+  springk = 30;
+  mass=5;
+  amplitude=.5;
+  mu=0;
+  spring = new Spring(createVector(10,height/2),springk,mass,250,amplitude,mu);
+
+  //spring = new Spring(createVector(10,height/2),30,5,250,.5,0);
   spring.rotation =0;
 
   dispv = new Arrow(start = new createVector(0,0),end = new createVector(0,0));
@@ -31,6 +51,8 @@ function draw() {
 background(255);
 
 
+
+  spring.setPlay(play);
 
 spring.update();
 spring.display();
@@ -70,4 +92,29 @@ accelv.target = spring.acceleration.mult(20).add(spring.displacement);
 accelv.color = color(230,20,230);
 accelv.display();
 pop();
+}
+
+
+
+function togglePlayButton(){
+  playButton.value = !playButton.value;
+  play = !play;
+  if (play) {
+    playButton.html("pause");
+  }
+  if (!play){
+    playButton.html("play");
+
+  }
+}
+function stopAn(){
+  play = false;
+
+
+}
+function startAn() {
+  if (playButton.value){
+    play = true;
+  }
+
 }
